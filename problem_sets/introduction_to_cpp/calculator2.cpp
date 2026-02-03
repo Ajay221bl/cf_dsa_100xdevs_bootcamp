f352bda1511913315bb488fe33184bbef5d65d1d
Ajaydeep <ajaydeep221b@gmail.com>
2026-02-03 21:47:06 +0530

    created calculator for calculating operations for really long numbers as well

---
+++ problem_sets/introduction_to_cpp/calculator2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    long long a;
+    long long b;
+    cin >> a;
+
+    cin >> b;
+    cout << a << " + " << b << " = " << a + b <<endl;
+    cout << "\n";
+    cout << a << " - " << b << " = " << a - b <<endl;
+    cout << "\n";
+    cout << a << " * " << b << " = " << a * b <<endl;
+    cout << "\n";
+    cout << a << " / " << b << " = " << a / b <<endl;
+    cout << "\n";
+    cout << a << " % " << b << " = " << a % b;
+}
