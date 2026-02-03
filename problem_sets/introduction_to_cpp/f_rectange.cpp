ed1bd0734d363dd623145605d258c70916383202
Ajaydeep <ajaydeep221b@gmail.com>
2026-02-03 20:48:51 +0530

    created new problem set directories

---
+++ problem_sets/introduction_to_cpp/f_rectange.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int length;
+    int breadth; 
+    cout << "Enter Rectangle Length:" << endl;
+    cin >>  length;
+    cout << "Enter Rectangle Breadth:" << endl;
+    cin >>  breadth;
+    cout << "Area = " << length * breadth << endl;
+    cout << "Perimeter = "<<  2 * (length + breadth) << endl ;
+}
