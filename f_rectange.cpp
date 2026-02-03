407f1583a7b243ea04759b4a85bd026c776d243b
Ajaydeep <ajaydeep221b@gmail.com>
2026-02-03 20:38:54 +0530

    calculating rectangle perimeter and area

---
+++ f_rectange.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int length;
+    int breadth; 
+    cout << "Enter Rectangle Length:" << endl;
+    cin >>  length;
+    cout << "Enter Rectangle Breadth:" << endl;
+    cin >>  breadth;
+    cout << "Area = " << length * breadth << endl;
+    cout << "Perimeter = "<<  2 * (length + breadth) << endl ;
+}
