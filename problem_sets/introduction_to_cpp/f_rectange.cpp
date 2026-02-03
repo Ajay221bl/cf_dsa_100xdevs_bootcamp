c2efbf5f84348d7a1461959731cb1a6f50bcf16e
Ajaydeep <ajaydeep221b@gmail.com>
2026-02-03 21:46:14 +0530

    removing the user focused strings in rectangle app

--- problem_sets/introduction_to_cpp/f_rectange.cpp
+++ problem_sets/introduction_to_cpp/f_rectange.cpp
@@ -4,9 +4,7 @@ using namespace std;
 int main() {
     int length;
     int breadth; 
-    cout << "Enter Rectangle Length:" << endl;
     cin >>  length;
-    cout << "Enter Rectangle Breadth:" << endl;
     cin >>  breadth;
     cout << "Area = " << length * breadth << endl;
     cout << "Perimeter = "<<  2 * (length + breadth) << endl ;
